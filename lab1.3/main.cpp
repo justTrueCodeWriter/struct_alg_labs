7ac5d9eb3e710d07aaa8d9dca0432b1c0e1a487b
justTrueCodeWriter <dmjustcoder8@gmail.com>
2023-09-20 21:07:51 +0300

    upd

--- lab1.3/main.cpp
+++ lab1.3/main.cpp
@@ -55,8 +55,62 @@ void alg_1() {
 
 }
 
+void alg_2() {
+	int amountElements;
+	printf("amountElements>>"); scanf("%d", &amountElements);
+
+	int arr[amountElements];
+	fill_array(arr, amountElements);	
+	print_array(arr, amountElements);
+
+	for (int i = 0; i < amountElements; i++) {
+		int tmp = 0;
+		for (int j = 0; j < amountElements-i-1; j++) {
+			if (arr[j] >= 0) {
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+
+		}
+	}
+	print_array(arr, amountElements);
+}
+
+int recursion(int n) {
+	
+	if (n != 0) {
+		return n;
+	}
+	else
+		return 0;
+
+}
+
+void alg_3() {
+	int amountElements;
+	printf("amountElements>>"); scanf("%d", &amountElements);
+
+	int arr[amountElements];
+	fill_array(arr, amountElements);	
+	print_array(arr, amountElements);
+
+	int tmp_index = 0;
+	for (int i = 0; i < amountElements; i++) {
+		int tmp = 0;
+		if (arr[i] < 0) {
+			tmp = arr[tmp_index];
+			arr[tmp_index] = arr[i];
+			arr[i] = tmp;
+			tmp_index = i;
+		}
+	}
+	print_array(arr, amountElements);
+
+}
+
 int main() {
 
-	alg_1();
+	alg_3();
 
 }
