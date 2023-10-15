99b47425b81f1fb8dd62e892d64f9ac934ef2bd6
justTrueCodeWriter <dmjustcoder8@gmail.com>
2023-10-15 18:36:44 +0300

    finished lab1.6

--- lab1.6/list.cpp
+++ lab1.6/list.cpp
@@ -2,11 +2,11 @@
 #include <malloc.h>
 #include "list.h"
 
-void push_front(List& list) {
+void push_front(List& list, int d) {
 
-	Student * e = (Student*)malloc(sizeof(Student));
+	Element * e = (Element*)malloc(sizeof(Element));
 
-	fill_element(*e);
+  e->d = d;
 
 	e->next = list.head;
 	list.head = e;
@@ -15,18 +15,18 @@ void push_front(List& list) {
 
 }
 
-void push_back(List& list) {
+void push_back(List& list, int d) {
 
-	Student * e = (Student*)malloc(sizeof(Student));
+	Element * e = (Element*)malloc(sizeof(Element));
 
-	fill_element(*e);
+  e->d = d; 
 
 	e->next = nullptr;
 
 	if (list.head == nullptr) 
 		list.head = e;
 	else {
-		Student * cur = list.head;
+		Element * cur = list.head;
 		while(cur->next != nullptr)
 			cur = cur->next;
 		cur->next = e;
@@ -35,7 +35,7 @@ void push_back(List& list) {
 
 }
 
-void push_position(int n, List& list) {
+void push_position(int n, List& list, int d) {
 
 	if (n <= 0) {
 		printf("INCORRECT!\n");
@@ -46,15 +46,15 @@ void push_position(int n, List& list) {
 		return;
 	}
 	if (n-1 == 0) {
-		push_front(list);
+		push_front(list, d);
 		return;
 	}
 	if (n-1 == list.size) {
-		push_back(list);
+		push_back(list, d);
 		return;
 	}
 	else {
-		Student* current = list.head;
+		Element* current = list.head;
 		for (int i = 0; i < n-2; i++) {
 			if (current == NULL) {
 				printf("INCORRECT!\n");
@@ -62,8 +62,8 @@ void push_position(int n, List& list) {
 			}
 			current = current->next;
 		}
-		Student* e = (Student*)malloc(sizeof(Student));
-		fill_element(*e);
+		Element* e = (Element*)malloc(sizeof(Element));
+    e->d = d;
 		e->next = current->next;
 		current->next = e;
 		list.size++;
@@ -71,98 +71,83 @@ void push_position(int n, List& list) {
 
 }
 
-void pull_front(List& list) {
+int pull_front(List& list) {
 
-	if (list.head == nullptr) return;
+  int d = list.head->d;
 
-	Student * e = list.head;
+	if (list.head == nullptr) return 0;
+
+	Element * e = list.head;
 	list.head = list.head->next;
 
 	free(e);
 
 	list.size--;
+  return d;
 }
 
-void pull_back(List& list) {
+int pull_back(List& list) {
 
-	if (list.head == NULL) return;
-	Student* cur = list.head;
+	if (list.head == NULL) return 0;
+	Element* cur = list.head;
 	if (list.head->next == NULL) {
 		list.head = NULL;
 		free(cur);
-		return;
+		return 0;
 	}
-	while (cur->next->next != NULL)
+	while (cur->next->next != NULL) {
 		cur = cur->next;
+  }
+  int d = cur->d;
 	free(cur->next);
 	cur->next = NULL;
 
 	list.size--;
+  return d;
 }
 
-void pull_position(int n, List& list) {
+int pull_position(int n, List& list) {
 
 	int tmp;
-	if (list.head == NULL) return; 
+	if (list.head == NULL) return 0; 
 	if (n <= 0) {
 		printf("INCORRECT!\n");
-		return;
+		return 0;
 	}
 	if (n-1 >= list.size) {
 		printf("INCORRECT\n");
-		return;
+		return 0;
 	}
 	if (n-1 == 0) {
 		pull_front(list);
-		return;
+		return 0;
 	}
 	else {
-		Student* current = list.head;
+		Element* current = list.head;
 		for (int i = 0; i < n-2; i++) {
 			if (current == NULL) {
 				printf("INCORRECT:\n");
-				return;
+				return 0;
 			}
 			current = current->next;
 		}
-		Student* next = current->next->next;
+    int d = current->next->d;
+		Element* next = current->next->next;
 		free(current->next);
 		current->next = next;
 		list.size--;
-		return;
+		return d;
 	}
-
-}
-
-void fill_element(Student& e) {
-
-	printf("Surname: "); scanf("%s", e.surname);			
-
-	printf("Year, Group: "); scanf("%d%d", &e.year, &e.group);	
-
-	printf("Marks: ");
-	scanf("%d", &e.mark);
-	
 
 }
 
 void print_list(List& list) {
 
 	if (list.head != nullptr) {
-
-		printf("------------------------------------------------\n");
-
-		for (Student * cur = list.head; cur != nullptr; cur = cur->next) {
-			printf("Surname: %s\n", cur->surname);
-			printf("Year: %i\n", cur->year);	
-			printf("Group: %i\n", cur->group);	
-
-			printf("Marks: ");
-			printf("%d ", cur->mark);
+		for (Element * cur = list.head; cur != nullptr; cur = cur->next) {
+			printf("%d ", cur->d);
 		}
-
-		printf("------------------------------------------------\n");
-
+    printf("\n");
 	}
 
 	else
@@ -174,7 +159,7 @@ void clear_list(List& list) {
 	if (list.head == NULL) return;
 
 	while(list.head != NULL) {
-		Student* e = list.head;	
+		Element* e = list.head;	
 		list.head = list.head->next;
 		free(e);
 	}
--- lab1.6/main.cpp
+++ lab1.6/main.cpp
@@ -1,239 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "list.h"
 
-#define BUFFER 100
+void fill_random(List& list, int n) {
 
-void list_operations();
+  for (int i = 0; i < n; i++) {
+    int data = ((rand() % (100-0)) + 0);
+    push_back(list, data);
+  }
 
-void average_group_mark(List& list);
+}
 
-void check_oldest_students(List& list);
-void check_youngest_students(List& list);
+void swap_elements(List& list, int n, int m) {
+  int data1;
+  int data2;
 
-void check_largest_group(List& list);
+  if (n == m) {
+    printf("INCORRECT!\n");
+    return;
+  }
+
+  if (n < m) {
+    data1 = pull_position(n, list);
+    data2 = pull_position(m-1, list);
+    print_list(list);
+    push_position(m-1, list, data1);
+    push_position(n, list, data2);
+
+    printf("%d %d\n", data1, data2);
+
+  }
+
+  else if (n > m) {
+    data1 = pull_position(n, list);
+    data2 = pull_position(m, list);
+    print_list(list);
+    push_position(m, list, data1);
+    push_position(n, list, data2);
+
+    printf("%d %d\n", data1, data2);
+  }
+
+}
 
 int main() {
 
-	list_operations();
+  srand(time(NULL));
 
-	return 0;
+  List list_field;
 
-}
-
-void list_operations() {
-
-	List list_field;
-
-	int userChoice = 0;
-
-	while (true) {
-		do {	
-			printf("Mode:\nPush front(1)\nPush back(2)\nPush position(3)\nPull front(4)\n\
-Pull back(5)\nPull position(6)\nAverage mark(7)\nLargest group(8)\nOldest & youngest(9)\nPrint list(10)\nClear list(11)\nExit(12)\n> "); scanf("%d", &userChoice);
-			switch (userChoice) {
-				case 1: push_front(list_field); break;
-				case 2: push_back(list_field); break;
-				case 3: {
-							int n;
-							printf("Position: "); scanf("%d", &n);
-							push_position(n, list_field); break;
-						}
-				case 4: pull_front(list_field); break;
-				case 5: pull_back(list_field); break;
-				case 6: {
-							int n;
-							printf("Position: "); scanf("%d", &n);
-							pull_position(n, list_field); 
-						} break;
-				case 7: average_group_mark(list_field);  break;
-				case 8: check_largest_group(list_field); break;
-				case 9: check_oldest_students(list_field); 
-						check_youngest_students(list_field);
-						break;
-				case 10:	print_list(list_field); break; 
-				case 11: clear_list(list_field); break;
-				case 12: clear_list(list_field); return; break;
-				default: printf("Incorrect choice!\n");
-			}
-		}while((userChoice>=1)&&(userChoice<=12));
-	}
-
-	
-
-}
-
-int group_amount(List& list) {
-
-	int groupsAmount = 0; 
-	int groupCopy = 0;
-
-	for (Student * cur = list.head; cur != nullptr; cur = cur->next) {
-		groupsAmount++;
-	}
-
-	printf("groupsAmount %d\n", groupsAmount);
-
-	for (Student * mainCheck = list.head; mainCheck != nullptr; mainCheck = mainCheck->next) {
-		for (Student * curCheck = mainCheck->next; curCheck != nullptr; curCheck = curCheck->next)	{
-					
-			if ((mainCheck->year == curCheck->year)&&(mainCheck->group == curCheck->group))	{
-				groupsAmount--;
-				break;
-			}
-		}
-
-	}
-
-	return groupsAmount;
-
-}
-
-void checkMaxYear(List& list, int &maxYear, int &maxGroup) {
-
-		
-	for (Student * mainCheck = list.head; mainCheck != nullptr; mainCheck = mainCheck->next) {
-		if (mainCheck->year > maxYear)		
-			maxYear = mainCheck->year;
-		if (mainCheck->group > maxGroup)
-			maxGroup = mainCheck->group;
-	}
-
-}
-
-void fill_arr_marks(float **marksMatrix, int maxYear, int maxGroup) {
-
-	for (int i = 0; i < maxYear; i++) {
-		for (int j = 0; j < maxGroup; j++) {
-			marksMatrix[i][j] = 0;
-		}
-
-	}
-
-}
-
-void fill_students_count(int **studentsCount, int maxYear, int maxGroup) {
-
-	for (int i = 0; i < maxYear; i++) {
-		for (int j = 0; j < maxGroup; j++) {
-			studentsCount[i][j] = 0;
-		}
-
-	}
-
-}
-
-void average_group_mark(List& list) {
-
-	int groupsAmount;
-
-	groupsAmount = group_amount(list);
-
-	printf("Group amount = %d\n", groupsAmount);
-
-	int maxYear = 0, maxGroup = 0;
-	checkMaxYear(list, maxYear, maxGroup);
-
-	float marksMatrix[maxYear][maxGroup];
-	int studentsCount[maxYear][maxGroup];
-	fill_arr_marks((float**)marksMatrix, maxYear, maxGroup);
-	fill_students_count((int**)studentsCount, maxYear, maxGroup);
-
-	for (Student * mainCheck = list.head; mainCheck != nullptr; mainCheck = mainCheck->next) {
-			marksMatrix[mainCheck->year][mainCheck->group] += mainCheck->mark;
-					studentsCount[mainCheck->year][mainCheck->group]++;
-	}
-
-	for (int i = 0; i < maxYear; i++) {
-		for (int j = 0; j < maxGroup; j++) {
-			if (marksMatrix[i][j] != 0)
-				printf("%f", marksMatrix[i][j]/studentsCount[i][j]);
-		}
-	}
-
-}
-
-void check_oldest_students(List& list) {
-
-	int oldest = 0; 
-
-	for (Student * mainCheck = list.head; mainCheck != nullptr; mainCheck = mainCheck->next) {
-		if (mainCheck->year > oldest) {
-			oldest = mainCheck->year;	
-		}
-	}
-
-	Student oldestStudents[BUFFER];
-	int i = 0, oldestCount = 0;
-	for (Student * cur = list.head; cur != nullptr; cur = cur->next) {
-	
-		if (cur->year == oldest) {
-			oldestStudents[i] = *cur;
-			oldestCount++;
-			i++;
-		}
-	}
-
-	printf("OLDEST:\n");
-
-	for (int j = 0; j<oldestCount; j++) {
-		printf("Surname: %s\nYear: %d\nGroup: %d\n", oldestStudents[j].surname, oldestStudents[j].year, oldestStudents[j].group);
-	}	
-	printf("\n");
-
-}
-
-void check_youngest_students(List& list) {
-
-	int youngest = 10; 
-
-	for (Student * mainCheck = list.head; mainCheck != nullptr; mainCheck = mainCheck->next) {
-		if (mainCheck->year < youngest) {
-			youngest = mainCheck->year;	
-		}
-	}
-
-	Student youngestStudents[BUFFER];
-	int i = 0, youngestCount = 0;
-	for (Student * cur = list.head; cur != nullptr; cur = cur->next) {
-	
-		if (cur->year == youngest) {
-			youngestStudents[i] = *cur;
-			youngestCount++;
-			i++;
-		}
-	}
-
-	printf("YOUNGEST:\n");
-
-	for (int j = 0; j<youngestCount; j++) {
-		printf("Surname: %s\nYear: %d\nGroup: %d\n", youngestStudents[j].surname, youngestStudents[j].year, youngestStudents[j].group);
-	}
-	printf("\n");
-
-}
-
-void check_largest_group(List& list) {
-
-	int groupSize = 0, largest = 0; 
-	int largestYear = 0, largestGroup = 0;
-
-	for (Student * mainCheck = list.head; mainCheck != nullptr; mainCheck = mainCheck->next) {
-		groupSize = 0;
-		for (Student * curCheck = list.head; curCheck != nullptr; curCheck = curCheck->next)	{
-					
-			if ((mainCheck->year == curCheck->year)&&(mainCheck->group == curCheck->group))	{
-				groupSize++;
-			}
-		}
-		
-		if (groupSize > largest) {
-			largest = groupSize;	
-			largestYear = mainCheck->year;
-			largestGroup = mainCheck->group;
-		}
-	}
-	//printf("largest=%d | checkedElemSize=%d\n", largest, checkedElemSize);
-	printf("Year: %d\nGroup: %d\n", largestYear, largestGroup);
+  fill_random(list_field, 10);
+  print_list(list_field); 
+  
+  int n, m;
+  printf("n = "); scanf("%d", &n);
+  printf("m = "); scanf("%d", &m);
+  
+  swap_elements(list_field, n, m);
+  print_list(list_field);
 
 }
